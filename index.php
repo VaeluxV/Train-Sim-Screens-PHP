88d273e2b9589d40e57084ff55d63edd29b2cea0
Subze <78231768+SubzeV@users.noreply.github.com>
2023-09-10 13:00:04 +0200

    test page and basic style

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Train Sim Screen version 10.09.23-01</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+    <h1>Test 123</h1>
+    <p>Paragraph</p>
+    <img src="img/circle.png" alt="circle" class="centerimg">
+
+</body>
+
+</html> 
