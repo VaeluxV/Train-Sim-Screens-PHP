ed7887d407703522c4999ee73adf371b86db4403
Subze <78231768+SubzeV@users.noreply.github.com>
2023-09-13 13:14:40 +0200

    Added placeholder page for universal screen

---
+++ trains/Any (universal generic screen).php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>TGV POS - TSScreen 10.09.23-02</title>
+    <link rel="stylesheet" href="/./css/style.css">
+    <link rel="stylesheet" href="/./css/styleindex.css">
+    <link rel="icon" type="image/x-icon" href="/./img/favicon.ico">
+</head>
+
+<body>
+
+    <div class="centerdiv">
+        <div class="aligndiv">
+            <img src="/./img/logo-full.png" alt="train-sim-screens logo" class="logoimg" />
+            <h1 class="headertext">Placeholder page</h1>
+        </div>
+
+        <div class="aligndiv">
+            <p class="copyright">Coming soon...</p>
+            <br>
+            <a class="trainlist" href="/./index.php">Back to home</a>
+            <br><br>
+            <p class="copyright">&copy;Subze_V</p>
+        </div>
+    </div>
+
+</body>
+
+</html>
+
+<script src="/./trains-js/TGV POS.js"> </script>
