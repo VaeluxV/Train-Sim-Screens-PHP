b17d76638838fa23b2bb5ceee50efb6058cd53a8
Subze <78231768+SubzeV@users.noreply.github.com>
2023-09-13 13:06:28 +0200

    Added TVM with debug to TGV POS

---
+++ trains-js/TGV POS.js
@@ -0,0 +1,353 @@
+function clearScreen() {
+    document.getElementById("square1").src = "/./img/numbers/empty_invis.png";
+    document.getElementById("square2").src = "/./img/numbers/empty_invis.png";
+    document.getElementById("square3").src = "/./img/numbers/empty_invis.png";
+}
+
+function change320G() {
+    document.getElementById("square1").src = "/./img/numbers/green3.png";
+    document.getElementById("square2").src = "/./img/numbers/green2.png";
+    document.getElementById("square3").src = "/./img/numbers/green0.png";
+}
+
+function change300G() {
+    document.getElementById("square1").src = "/./img/numbers/green3.png";
+    document.getElementById("square2").src = "/./img/numbers/green0.png";
+    document.getElementById("square3").src = "/./img/numbers/green0.png";
+}
+
+function change270G() {
+    document.getElementById("square1").src = "/./img/numbers/green2.png";
+    document.getElementById("square2").src = "/./img/numbers/green7.png";
+    document.getElementById("square3").src = "/./img/numbers/green0.png";
+}
+
+function change230G() {
+    document.getElementById("square1").src = "/./img/numbers/green2.png";
+    document.getElementById("square2").src = "/./img/numbers/green3.png";
+    document.getElementById("square3").src = "/./img/numbers/green0.png";
+}
+
+function change200G() {
+    document.getElementById("square1").src = "/./img/numbers/green2.png";
+    document.getElementById("square2").src = "/./img/numbers/green0.png";
+    document.getElementById("square3").src = "/./img/numbers/green0.png";
+}
+
+function change170G() {
+    document.getElementById("square1").src = "/./img/numbers/green1.png";
+    document.getElementById("square2").src = "/./img/numbers/green7.png";
+    document.getElementById("square3").src = "/./img/numbers/green0.png";
+}
+
+function change160G() {
+    document.getElementById("square1").src = "/./img/numbers/green1.png";
+    document.getElementById("square2").src = "/./img/numbers/green6.png";
+    document.getElementById("square3").src = "/./img/numbers/green0.png";
+}
+
+function change130G() {
+    document.getElementById("square1").src = "/./img/numbers/green1.png";
+    document.getElementById("square2").src = "/./img/numbers/green3.png";
+    document.getElementById("square3").src = "/./img/numbers/green0.png";
+}
+
+function change100G() {
+    document.getElementById("square1").src = "/./img/numbers/green1.png";
+    document.getElementById("square2").src = "/./img/numbers/green0.png";
+    document.getElementById("square3").src = "/./img/numbers/green0.png";
+}
+
+function change080G() {
+    document.getElementById("square1").src = "/./img/numbers/green0.png";
+    document.getElementById("square2").src = "/./img/numbers/green8.png";
+    document.getElementById("square3").src = "/./img/numbers/green0.png";
+}
+
+function change320Gblink() {
+    document.getElementById("square1").src = "/./img/numbers/green3blink.png";
+    document.getElementById("square2").src = "/./img/numbers/green2blink.png";
+    document.getElementById("square3").src = "/./img/numbers/green0blink.png";
+}
+
+function change300Gblink() {
+    document.getElementById("square1").src = "/./img/numbers/green3blink.png";
+    document.getElementById("square2").src = "/./img/numbers/green0blink.png";
+    document.getElementById("square3").src = "/./img/numbers/green0blink.png";
+}
+
+function change270Gblink() {
+    document.getElementById("square1").src = "/./img/numbers/green2blink.png";
+    document.getElementById("square2").src = "/./img/numbers/green7blink.png";
+    document.getElementById("square3").src = "/./img/numbers/green0blink.png";
+}
+
+function change230Gblink() {
+    document.getElementById("square1").src = "/./img/numbers/green2blink.png";
+    document.getElementById("square2").src = "/./img/numbers/green3blink.png";
+    document.getElementById("square3").src = "/./img/numbers/green0blink.png";
+}
+
+function change200Gblink() {
+    document.getElementById("square1").src = "/./img/numbers/green2blink.png";
+    document.getElementById("square2").src = "/./img/numbers/green0blink.png";
+    document.getElementById("square3").src = "/./img/numbers/green0blink.png";
+}
+
+function change170Gblink() {
+    document.getElementById("square1").src = "/./img/numbers/green1blink.png";
+    document.getElementById("square2").src = "/./img/numbers/green7blink.png";
+    document.getElementById("square3").src = "/./img/numbers/green0blink.png";
+}
+
+function change160Gblink() {
+    document.getElementById("square1").src = "/./img/numbers/green1blink.png";
+    document.getElementById("square2").src = "/./img/numbers/green6blink.png";
+    document.getElementById("square3").src = "/./img/numbers/green0blink.png";
+}
+
+function change130Gblink() {
+    document.getElementById("square1").src = "/./img/numbers/green1blink.png";
+    document.getElementById("square2").src = "/./img/numbers/green3blink.png";
+    document.getElementById("square3").src = "/./img/numbers/green0blink.png";
+}
+
+function change100Gblink() {
+    document.getElementById("square1").src = "/./img/numbers/green1blink.png";
+    document.getElementById("square2").src = "/./img/numbers/green0blink.png";
+    document.getElementById("square3").src = "/./img/numbers/green0blink.png";
+}
+
+function change080Gblink() {
+    document.getElementById("square1").src = "/./img/numbers/green0blink.png";
+    document.getElementById("square2").src = "/./img/numbers/green8blink.png";
+    document.getElementById("square3").src = "/./img/numbers/green0blink.png";
+}
+
+function change300B() {
+    document.getElementById("square1").src = "/./img/numbers/black3.png";
+    document.getElementById("square2").src = "/./img/numbers/black0.png";
+    document.getElementById("square3").src = "/./img/numbers/black0.png";
+}
+
+function change270B() {
+    document.getElementById("square1").src = "/./img/numbers/black2.png";
+    document.getElementById("square2").src = "/./img/numbers/black7.png";
+    document.getElementById("square3").src = "/./img/numbers/black0.png";
+}
+
+function change230B() {
+    document.getElementById("square1").src = "/./img/numbers/black2.png";
+    document.getElementById("square2").src = "/./img/numbers/black3.png";
+    document.getElementById("square3").src = "/./img/numbers/black0.png";
+}
+
+function change200B() {
+    document.getElementById("square1").src = "/./img/numbers/black2.png";
+    document.getElementById("square2").src = "/./img/numbers/black0.png";
+    document.getElementById("square3").src = "/./img/numbers/black0.png";
+}
+
+function change170B() {
+    document.getElementById("square1").src = "/./img/numbers/black1.png";
+    document.getElementById("square2").src = "/./img/numbers/black7.png";
+    document.getElementById("square3").src = "/./img/numbers/black0.png";
+}
+
+function change160B() {
+    document.getElementById("square1").src = "/./img/numbers/black1.png";
+    document.getElementById("square2").src = "/./img/numbers/black6.png";
+    document.getElementById("square3").src = "/./img/numbers/black0.png";
+}
+
+function change130B() {
+    document.getElementById("square1").src = "/./img/numbers/black1.png";
+    document.getElementById("square2").src = "/./img/numbers/black3.png";
+    document.getElementById("square3").src = "/./img/numbers/black0.png";
+}
+
+function change100B() {
+    document.getElementById("square1").src = "/./img/numbers/black1.png";
+    document.getElementById("square2").src = "/./img/numbers/black0.png";
+    document.getElementById("square3").src = "/./img/numbers/black0.png";
+}
+
+function change080B() {
+    document.getElementById("square1").src = "/./img/numbers/black0.png";
+    document.getElementById("square2").src = "/./img/numbers/black8.png";
+    document.getElementById("square3").src = "/./img/numbers/black0.png";
+}
+
+function change300Bblink() {
+    document.getElementById("square1").src = "/./img/numbers/black3blink.png";
+    document.getElementById("square2").src = "/./img/numbers/black0blink.png";
+    document.getElementById("square3").src = "/./img/numbers/black0blink.png";
+}
+
+function change270Bblink() {
+    document.getElementById("square1").src = "/./img/numbers/black2blink.png";
+    document.getElementById("square2").src = "/./img/numbers/black7blink.png";
+    document.getElementById("square3").src = "/./img/numbers/black0blink.png";
+}
+
+function change230Bblink() {
+    document.getElementById("square1").src = "/./img/numbers/black2blink.png";
+    document.getElementById("square2").src = "/./img/numbers/black3blink.png";
+    document.getElementById("square3").src = "/./img/numbers/black0blink.png";
+}
+
+function change200Bblink() {
+    document.getElementById("square1").src = "/./img/numbers/black2blink.png";
+    document.getElementById("square2").src = "/./img/numbers/black0blink.png";
+    document.getElementById("square3").src = "/./img/numbers/black0blink.png";
+}
+
+function change170Bblink() {
+    document.getElementById("square1").src = "/./img/numbers/black1blink.png";
+    document.getElementById("square2").src = "/./img/numbers/black7blink.png";
+    document.getElementById("square3").src = "/./img/numbers/black0blink.png";
+}
+
+function change160Bblink() {
+    document.getElementById("square1").src = "/./img/numbers/black1blink.png";
+    document.getElementById("square2").src = "/./img/numbers/black6blink.png";
+    document.getElementById("square3").src = "/./img/numbers/black0blink.png";
+}
+
+function change130Bblink() {
+    document.getElementById("square1").src = "/./img/numbers/black1blink.png";
+    document.getElementById("square2").src = "/./img/numbers/black3blink.png";
+    document.getElementById("square3").src = "/./img/numbers/black0blink.png";
+}
+
+function change100Bblink() {
+    document.getElementById("square1").src = "/./img/numbers/black1blink.png";
+    document.getElementById("square2").src = "/./img/numbers/black0blink.png";
+    document.getElementById("square3").src = "/./img/numbers/black0blink.png";
+}
+
+function change080Bblink() {
+    document.getElementById("square1").src = "/./img/numbers/black0blink.png";
+    document.getElementById("square2").src = "/./img/numbers/black8blink.png";
+    document.getElementById("square3").src = "/./img/numbers/black0blink.png";
+}
+
+function change300W() {
+    document.getElementById("square1").src = "/./img/numbers/white3.png";
+    document.getElementById("square2").src = "/./img/numbers/white0.png";
+    document.getElementById("square3").src = "/./img/numbers/white0.png";
+}
+
+function change270W() {
+    document.getElementById("square1").src = "/./img/numbers/white2.png";
+    document.getElementById("square2").src = "/./img/numbers/white7.png";
+    document.getElementById("square3").src = "/./img/numbers/white0.png";
+}
+
+function change230W() {
+    document.getElementById("square1").src = "/./img/numbers/white2.png";
+    document.getElementById("square2").src = "/./img/numbers/white3.png";
+    document.getElementById("square3").src = "/./img/numbers/white0.png";
+}
+
+function change200W() {
+    document.getElementById("square1").src = "/./img/numbers/white2.png";
+    document.getElementById("square2").src = "/./img/numbers/white0.png";
+    document.getElementById("square3").src = "/./img/numbers/white0.png";
+}
+
+function change170W() {
+    document.getElementById("square1").src = "/./img/numbers/white1.png";
+    document.getElementById("square2").src = "/./img/numbers/white7.png";
+    document.getElementById("square3").src = "/./img/numbers/white0.png";
+}
+
+function change160W() {
+    document.getElementById("square1").src = "/./img/numbers/white1.png";
+    document.getElementById("square2").src = "/./img/numbers/white6.png";
+    document.getElementById("square3").src = "/./img/numbers/white0.png";
+}
+
+function change130W() {
+    document.getElementById("square1").src = "/./img/numbers/white1.png";
+    document.getElementById("square2").src = "/./img/numbers/white3.png";
+    document.getElementById("square3").src = "/./img/numbers/white0.png";
+}
+
+function change100W() {
+    document.getElementById("square1").src = "/./img/numbers/white1.png";
+    document.getElementById("square2").src = "/./img/numbers/white0.png";
+    document.getElementById("square3").src = "/./img/numbers/white0.png";
+}
+
+function change080W() {
+    document.getElementById("square1").src = "/./img/numbers/white0.png";
+    document.getElementById("square2").src = "/./img/numbers/white8.png";
+    document.getElementById("square3").src = "/./img/numbers/white0.png";
+}
+
+function change300Wblink() {
+    document.getElementById("square1").src = "/./img/numbers/white3blink.png";
+    document.getElementById("square2").src = "/./img/numbers/white0blink.png";
+    document.getElementById("square3").src = "/./img/numbers/white0blink.png";
+}
+
+function change270Wblink() {
+    document.getElementById("square1").src = "/./img/numbers/white2blink.png";
+    document.getElementById("square2").src = "/./img/numbers/white7blink.png";
+    document.getElementById("square3").src = "/./img/numbers/white0blink.png";
+}
+
+function change230Wblink() {
+    document.getElementById("square1").src = "/./img/numbers/white2blink.png";
+    document.getElementById("square2").src = "/./img/numbers/white3blink.png";
+    document.getElementById("square3").src = "/./img/numbers/white0blink.png";
+}
+
+function change200Wblink() {
+    document.getElementById("square1").src = "/./img/numbers/white2blink.png";
+    document.getElementById("square2").src = "/./img/numbers/white0blink.png";
+    document.getElementById("square3").src = "/./img/numbers/white0blink.png";
+}
+
+function change170Wblink() {
+    document.getElementById("square1").src = "/./img/numbers/white1blink.png";
+    document.getElementById("square2").src = "/./img/numbers/white7blink.png";
+    document.getElementById("square3").src = "/./img/numbers/white0blink.png";
+}
+
+function change160Wblink() {
+    document.getElementById("square1").src = "/./img/numbers/white1blink.png";
+    document.getElementById("square2").src = "/./img/numbers/white6blink.png";
+    document.getElementById("square3").src = "/./img/numbers/white0blink.png";
+}
+
+function change130Wblink() {
+    document.getElementById("square1").src = "/./img/numbers/white1blink.png";
+    document.getElementById("square2").src = "/./img/numbers/white3blink.png";
+    document.getElementById("square3").src = "/./img/numbers/white0blink.png";
+}
+
+function change100Wblink() {
+    document.getElementById("square1").src = "/./img/numbers/white1blink.png";
+    document.getElementById("square2").src = "/./img/numbers/white0blink.png";
+    document.getElementById("square3").src = "/./img/numbers/white0blink.png";
+}
+
+function change080Wblink() {
+    document.getElementById("square1").src = "/./img/numbers/white0blink.png";
+    document.getElementById("square2").src = "/./img/numbers/white8blink.png";
+    document.getElementById("square3").src = "/./img/numbers/white0blink.png";
+}
+
+function change0R() {
+    document.getElementById("square1").src = "/./img/numbers/red0.png";
+    document.getElementById("square2").src = "/./img/numbers/red0.png";
+    document.getElementById("square3").src = "/./img/numbers/red0.png";
+}
+
+function changeR() {
+    document.getElementById("square1").src = "/./img/numbers/red.png";
+    document.getElementById("square2").src = "/./img/numbers/red.png";
+    document.getElementById("square3").src = "/./img/numbers/red.png";
+}
--- trains/TGV POS.php
+++ trains/TGV POS.php
@@ -17,6 +17,98 @@
     
     <img src="/./img/tgv-pos/gauge-tgv.png" alt="gauge" class="centerimg">
 
+    <div class="tvmblock">
+        <div class="grid">
+            <div class="square"> <img id="square1" src="/./img/numbers/empty_invis.png"> </div>
+            <div class="square"> <img id="square2" src="/./img/numbers/empty_invis.png"> </div>
+            <div class="square"> <img id="square3" src="/./img/numbers/empty_invis.png"> </div>
+        </div>
+    </div>
+    
+    <br>
+
+    <button onclick="change320G()">Set 320 green</button>
+    <button onclick="change300G()">Set 300 green</button>
+    <button onclick="change270G()">Set 270 green</button>
+    <button onclick="change230G()">Set 230 green</button>
+    <button onclick="change200G()">Set 200 green</button>
+    <button onclick="change170G()">Set 170 green</button>
+    <button onclick="change160G()">Set 160 green</button>
+    <button onclick="change130G()">Set 130 green</button>
+    <button onclick="change100G()">Set 100 green</button>
+    <button onclick="change080G()">Set 080 green</button>
+
+    <br><br>
+
+    <button onclick="change320Gblink()">Set 320 green blink</button>
+    <button onclick="change300Gblink()">Set 300 green blink</button>
+    <button onclick="change270Gblink()">Set 270 green blink</button>
+    <button onclick="change230Gblink()">Set 230 green blink</button>
+    <button onclick="change200Gblink()">Set 200 green blink</button>
+    <button onclick="change170Gblink()">Set 170 green blink</button>
+    <button onclick="change160Gblink()">Set 160 green blink</button>
+    <button onclick="change130Gblink()">Set 130 green blink</button>
+    <button onclick="change100Gblink()">Set 100 green blink</button>
+    <button onclick="change080Gblink()">Set 080 green blink</button>
+
+    <br><br>
+
+    <button onclick="change300B()">Set 300 black</button>
+    <button onclick="change270B()">Set 270 black</button>
+    <button onclick="change230B()">Set 230 black</button>
+    <button onclick="change200B()">Set 200 black</button>
+    <button onclick="change170B()">Set 170 black</button>
+    <button onclick="change160B()">Set 160 black</button>
+    <button onclick="change130B()">Set 130 black</button>
+    <button onclick="change100B()">Set 100 black</button>
+    <button onclick="change080B()">Set 080 black</button>
+
+    <br><br>
+
+    <button onclick="change300Bblink()">Set 300 black blink</button>
+    <button onclick="change270Bblink()">Set 270 black blink</button>
+    <button onclick="change230Bblink()">Set 230 black blink</button>
+    <button onclick="change200Bblink()">Set 200 black blink</button>
+    <button onclick="change170Bblink()">Set 170 black blink</button>
+    <button onclick="change160Bblink()">Set 160 black blink</button>
+    <button onclick="change130Bblink()">Set 130 black blink</button>
+    <button onclick="change100Bblink()">Set 100 black blink</button>
+    <button onclick="change080Bblink()">Set 080 black blink</button>
+
+    <br><br>
+
+    <button onclick="change300W()">Set 300 white</button>
+    <button onclick="change270W()">Set 270 white</button>
+    <button onclick="change230W()">Set 230 white</button>
+    <button onclick="change200W()">Set 200 white</button>
+    <button onclick="change170W()">Set 170 white</button>
+    <button onclick="change160W()">Set 160 white</button>
+    <button onclick="change130W()">Set 130 white</button>
+    <button onclick="change100W()">Set 100 white</button>
+    <button onclick="change080W()">Set 080 white</button>
+
+    <br><br>
+
+    <button onclick="change300Wblink()">Set 300 white blink</button>
+    <button onclick="change270Wblink()">Set 270 white blink</button>
+    <button onclick="change230Wblink()">Set 230 white blink</button>
+    <button onclick="change200Wblink()">Set 200 white blink</button>
+    <button onclick="change170Wblink()">Set 170 white blink</button>
+    <button onclick="change160Wblink()">Set 160 white blink</button>
+    <button onclick="change130Wblink()">Set 130 white blink</button>
+    <button onclick="change100Wblink()">Set 100 white blink</button>
+    <button onclick="change080Wblink()">Set 080 white blink</button>
+
+    <br><br>
+
+    <button onclick="change0R()">Set stop 0 red</button>
+
+    <button onclick="changeR()">Set stop full red</button>
+
+    <button onclick="clearScreen()">Turn off TVM</button>
+
 </body>
 
-</html> 
+</html>
+
+<script src="/./trains-js/TGV POS.js"> </script>
